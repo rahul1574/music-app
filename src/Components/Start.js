26c7ff4172fd849b31e8964f8ab63cf736f1477f
rahul1574 <166401965+rahul1574@users.noreply.github.com>
2024-08-05 00:05:05 +0530

    Update Start.js

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 // import React,{useState,useEffect} from 'react'
-import{Link} from "react-router-dom";
+// import{Link} from "react-router-dom";
 import ReactConfetti from 'react-confetti'
 function Start() {
     const removestart=()=>{
