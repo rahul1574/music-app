26ced6c4efec2076e2906e7c17992c30ae74077b
rahul1574 <166401965+rahul1574@users.noreply.github.com>
2024-08-05 17:15:31 +0530

    Update Player5.js

--- src/Components/Player5.js
+++ src/Components/Player5.js
@@ -12,7 +12,8 @@ const AudioPlayer = () => {
     const [duration, setDuration] = useState(0);
     const [loopEnabled, setLoopEnabled] = useState(false);
     const playlist = [
-        { title: "HOSAAHOREY",cast:"Ananth Sreeram,K.K., Lesle Lewis", image:"image5.jpeg",src: "song6.mp3",lyrics:`
+        { title: "NEETI MULLI",cast:"Sagar, Sumangaly", image:"imagep51.jpg",src: "songp51.mp3",lyrics:''},
+            {title: "HOSAAHOREY",cast:"Ananth Sreeram,K.K., Lesle Lewis", image:"image5.jpeg",src: "song6.mp3",lyrics:`
         Hosahare..... Hosahore.....
         Saadhinche sattuva unnadhi gundelalo..
         Preminche mettani manasula moolalalo...
